18b184d3ae65f2d42687b7f8dd699150fc9c4fac
whatislife <1053400192@qq.com>
2018-06-19 16:29:44 +0800

    添加注释文件

--- src/main/java/com/springmvc/controller/HelloController.java
+++ src/main/java/com/springmvc/controller/HelloController.java
@@ -15,37 +15,60 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
-
-
-//url:  http://localhost:8080/HelloSpringMVC/hello/mvc
-
-@Controller  //告诉DispatcherServlet相关的容器， 这是一个Controller，
-@RequestMapping(value="/hello")  //类级别的RequestMapping，告诉DispatcherServlet由这个类负责处理以及URL。HandlerMapping依靠这个标签来工作
+/**
+ * 
+* @ClassName: HelloController  
+* <p>Description: 单个文件、多个文件上传下载  </p>
+* http://localhost:8080/HelloSpringMVC/hello/mvc
+* 类级别的RequestMapping，告诉DispatcherServlet由这个类负责处理以及URL。HandlerMapping依靠这个标签来工作
+* @date 2018年6月19日 下午4:23:30  
+*
+ */
+@Controller  
+@RequestMapping(value="/hello")  
 public class HelloController {
 	
 	private static Logger log = LoggerFactory.getLogger(HelloController.class);
 		
-	//方法级别的RequestMapping， 限制并缩小了URL路径匹配，同类级别的标签协同工作，最终确定拦截到的URL由那个方法处理
-	//并指定访问方法为GET
+	
+	    /**
+	     * 
+	    * <p>Title: HelloWorld  </p>
+	    * Description: 
+	    * 方法级别的RequestMapping， 限制并缩小了URL路径匹配，同类级别的标签协同工作，最终确定拦截到的URL由那个方法处理
+	    * 并指定访问方法为GET
+	    * 视图渲染，/WEB-INF/view/HelloWorld.jsp
+	    * @date 2018年6月19日 下午4:24:47  
+	    * @param model
+	    * @return
+	     */
 	    @RequestMapping(value="/mvc",method=RequestMethod.GET)  
 	    public String HelloWorld(Model model){  
 		  
 	        model.addAttribute("message","Hello Spring MVC!!!");  //传参数给前端
-	        
-	      //视图渲染，/WEB-INF/view/HelloWorld.jsp
-	        return "HelloWorld";  //页面的名称，根据此字符串会去寻找名为HelloWorld.jsp的页面
+
+	        return "HelloWorld";  
 	    }  
 	    
-	  //定位到上传文件界面 /hello/upload
+	    /**
+	     * 
+	    * <p>Title: showUploadPage  </p>
+	    * Description: 定位到上传文件界面 /hello/upload
+	    * @date 2018年6月19日 下午4:24:25  
+	    * @return
+	     */
 		@RequestMapping(value="/upload", method=RequestMethod.GET)
 		public String showUploadPage(){	
-			return "uploadFile";		 //上传单个文件
+			return "uploadFile";
 		}
 		
 		/**
-		 * 上传单个文件操作
-		 * @param multi
-		 * @return
+		 * 
+		* <p>Title: doUploadFile  </p>
+		* Description: 上传单个文件操作  这里将上传得到的文件保存至 d:\\temp\\file 目录
+		* @date 2018年6月19日 下午4:27:27  
+		* @param file
+		* @return
 		 */
 		@RequestMapping(value="/doUpload", method=RequestMethod.POST)
 		public String doUploadFile(@RequestParam("file") MultipartFile file){
@@ -53,30 +76,37 @@ public class HelloController {
 			if(!file.isEmpty()){
 				log.debug("Process file: {}", file.getOriginalFilename());
 				try {
-					//这里将上传得到的文件保存至 d:\\temp\\file 目录
 					FileUtils.copyInputStreamToFile(file.getInputStream(), new File("d:\\temp\\file\\", 
 							System.currentTimeMillis()+ file.getOriginalFilename()));
 				} catch (IOException e) {
 					e.printStackTrace();
 					log.error(e.toString());
 				}
+				
 			}
 
 			return "success";
 		}
-		
-		//定位到上传多个文件界面 /hello/uploadMulti
+		/**
+		 * 
+		* <p>Title: showUploadPage2  </p>
+		* Description: 多个文件上传界面 /hello/uploadMulti  view文件夹下的上传多个文件的页面
+		* @date 2018年6月19日 下午4:26:26  
+		* @return
+		 */
 		@RequestMapping(value="/uploadMulti", method=RequestMethod.GET)
 		public String showUploadPage2(){	
-			return "uploadMultifile";		 //view文件夹下的上传多个文件的页面
+			return "uploadMultifile";
 		}
 		
-		
 		/**
-		 * 上传多个附件的操作类
-		 * @param multiRequest
-		 * @return
-		 * @throws IOException
+		 * 
+		* <p>Title: doUploadFile2  </p>
+		* Description: 上传多个附件的操作类
+		* @date 2018年6月19日 下午4:27:00  
+		* @param multiRequest
+		* @return
+		* @throws IOException
 		 */
 		@RequestMapping(value="/doMultiUpload", method=RequestMethod.POST)
 		public String doUploadFile2(MultipartHttpServletRequest multiRequest) throws IOException{
@@ -92,9 +122,7 @@ public class HelloController {
 				}
 
 			}
-
 			return "success";
 		}
-		
 		
 }
